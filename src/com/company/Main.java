1b1f5892b0dd1d1bb74487dc1b5c7117212dd007
dam2a <ppatel77@elpuig.xeill.net>
2019-10-25 18:39:56 +0200

    codigo_heco

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,44 @@
 package com.company;
 
+import jdk.nashorn.api.tree.WhileLoopTree;
+
 import javax.crypto.SecretKey;
+import java.io.*;
+import java.nio.file.Files;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
         String usuario = "Hola Mundo¡";
-            Codigo_clave codigo_clave = new Codigo_clave();
+        String pass = "usuario";
+        Codigo_clave codigo_clave = new Codigo_clave();
 
-            codigo_clave.keygenKeyGeneration(256);
-            codigo_clave.passwordKeyGeneration("pratik", 256);
-            codigo_clave.encryptData();
+//        // Ejercicio 1
+//            //SecretKey sk = codigo_clave.keygenKeyGeneration(256);
+//
+//            SecretKey sk = codigo_clave.passwordKeyGeneration(pass, 256);
+//
+//            byte [] xif = codigo_clave.encryptData(sk, usuario.getBytes());
+//
+//            //codigo_clave.passwordKeyGeneration(pass, 256);
+//
+//            byte [] dexif = codigo_clave.decryptData(sk, xif);
+//
+//            System.out.println(new String(dexif));
 
 
+            // Ejercicio 2
+            String file = "/home/dam2a/Baixades/claves.txt";
+            String clave;
+            byte[] fileencryp = Files.readAllBytes(new File(file).toPath());
+            BufferedReader b = new BufferedReader(new FileReader(new File("/home/dam2a/Baixades/textamagat")));
+
+            while ((clave = b.readLine()) != null){
+                SecretKey sKey = codigo_clave.passwordKeyGeneration(clave, 128);
+                if (codigo_clave.decryptData(sKey, fileencryp) != null){
+                    System.out.println(new String(codigo_clave.decryptData(sKey, fileencryp)));
+                }
+            }
     }
 }
--- src/com/company/Textamagat.java
+++ src/com/company/Textamagat.java
@@ -1,36 +1,20 @@
 package com.company;
 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
-public class Codigo_clave {
+public class Textamagat {
 
 
-//    public static SecretKey keygenKeyGeneration(int keySize) {
-//        SecretKey sKey = null;
-//        if ((keySize == 128)||(keySize == 192)||(keySize == 256)) {
-//            try {
-//                KeyGenerator kgen = KeyGenerator.getInstance("AES");
-//                kgen.init(keySize);
-//                sKey = kgen.generateKey();
-//
-//            } catch (NoSuchAlgorithmException ex) {
-//                System.err.println("Generador no disponible.");
-//            }
-//        }
-//        return sKey;
-//    }
 
     public SecretKey passwordKeyGeneration(String text, int keySize) {
         SecretKey sKey = null;
         if ((keySize == 128)||(keySize == 192)||(keySize == 256)) {
             try {
-                byte[] data = text.getBytes("UTF-8");
+                byte[] data = text.getBytes("UTF8");
                 MessageDigest md = MessageDigest.getInstance("SHA-256");
                 byte[] hash = md.digest(data);
                 byte[] key = Arrays.copyOf(hash, keySize/8);
@@ -54,15 +38,4 @@ public class Codigo_clave {
         return encryptedData;
     }
 
-    public static byte[] decryptData(SecretKey sKey, byte[] data) {
-        byte[] decryptedData = null;
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.DECRYPT_MODE, sKey);
-            decryptedData =  cipher.doFinal(data);
-        } catch (Exception  ex) {
-            System.err.println("Error dexifrant les dades: " + ex);
-        }
-        return decryptedData;
-    }
 }
